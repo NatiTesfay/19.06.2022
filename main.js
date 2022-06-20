2e36356e0a9bb934a9c49bc68e9200ee3efdda22
NatiTesfay <netaneltes66@gmail.com>
2022-06-20 17:36:49 +0300

    Class-js

---
+++ main.js
@@ -0,0 +1,76 @@
+// class Student{
+//     constructor(firstname,classroom , id){
+//     this.firstname = firstname;
+//     this.classroom = classroom;
+//     this.id = id;
+//     }
+// }
+// let user = new Student('netanel','12 grade' , 208512897);
+// let studentUser = new Student('Chen','10 grade' , 582103651);
+// console.log(user);
+// console.log(studentUser);
+
+// class Person{
+//     constructor(firstUserName,lastName,age,email){
+//         this.firstUserName = firstUserName;
+//         this.lastName = lastName;
+//         this.age = age;
+//         this.email = email;
+//     }
+// }
+// let userPerson = new Person('Netanel','Tesfay',25,'netaneltes@gmail.com');
+// let userPerson1 = new Person('Chen','Semo',30,'CHensemo@gmail.com');
+// let userPerson2 = new Person('Lior','Dalada',28,'DanielMalada@gmail.com');
+
+// for( let item in userPerson){
+//     document.write(userPerson[item] + " ") ;
+// }
+// document.body.innerHTML += "</br>"
+// for( let item in userPerson1){
+//     document.write(userPerson1[item] + " ");
+    
+// }
+// document.body.innerHTML += "</br>"
+// for( let item in userPerson2){
+//     document.write(userPerson2[item] + " ");
+// }
+
+
+
+class User{
+    constructor(firstName,lastName,age){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+}
+
+function btnPrint(){
+ const userInfo = document.getElementById('dateFromUser');
+const userInfo2 = document.getElementById('dateFromUser1');
+const userInfo3 = document.getElementById('dateFromUser2');
+
+const user2 = new User(firstName,lastName,age)
+console.log(user2)
+for(const item in user2){
+
+
+tableBody.appechild(document.createElement('th'))
+th.innerHTML = key + ":" + " ";
+th.innerHTML = '<td>${inputobject[key]}</td>'
+}
+
+}
+
+// document.body.innerHTML += "<table>" +  "<th>" + userInfo.value + "</th>" + "<tr>" + "<tb>" + userInfo2.value + "</tb>" + "<tb>" + userInfo3.value +"</tb>" + "</tr>" + "</table>";
+
+// function userDates() {
+//     console.log(userInfo.value , userInfo2.value , userInfo3.value);
+// }
+
+
+
+
+        // userInfo.push(first);
+        // userInfo.push(second);
+        // userInfo.push(third);
